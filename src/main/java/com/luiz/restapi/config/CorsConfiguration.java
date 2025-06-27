92874df55efae6b26a3a88b0352ac408dec530f3
LuizzJS <luizz.developer@gmail.com>
2025-06-27 13:48:54 +0200

    Changed mvn install to local maven

--- src/main/java/com/luiz/restapi/config/CorsConfiguration.java
+++ src/main/java/com/luiz/restapi/config/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(@SuppressWarnings("NullableProblems") CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
